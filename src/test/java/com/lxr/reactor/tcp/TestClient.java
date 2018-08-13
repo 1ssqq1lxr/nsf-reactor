2af2de900560e278a13a7e0787a4158c095e4f6e
luxurong <1074455781@qq.com>
2018-08-13 22:30:19 +0800

    初始化测试reactor-netty

--- src/test/java/com/lxr/reactor/tcp/TestClient.java
+++ src/test/java/com/lxr/reactor/tcp/TestClient.java
@@ -8,6 +8,7 @@ import reactor.ipc.netty.options.ServerOptions;
 import reactor.ipc.netty.tcp.TcpClient;
 
 import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
 import java.util.function.Consumer;
 
 public class TestClient {
@@ -15,20 +16,31 @@ public class TestClient {
     @Test
     public void startClient()
     {
+
         Consumer<? super ClientOptions.Builder<?>> opsHandler= ops-> ops.host("localhost").port(10008).afterNettyContextInit(afterChannelInit->{
             afterChannelInit
                     .addHandler("decode",new TcpDecoder(1024 * 1024*10,1,2,0,0))
                     .addHandler("encode",new TcpEncoder());
+
         });
 
         TcpClient.create(opsHandler)
-                .startAndAwait((in,out)->{
+                .newHandler((in,out)->{
                     in.receiveObject().map(obj->"client测试结果=========="+obj).subscribe(System.out::println);
                     in.onReadIdle(2000,()->{
-                        out.sendObject(Data.builder().header((byte)1).body(UUID.randomUUID().toString()).build()).then().subscribe();
+                        out.context().channel().writeAndFlush(Data.builder().header((byte)1).body(UUID.randomUUID().toString()).build());
+//                        out.sendObject(Data.builder().header((byte)1).body(UUID.randomUUID().toString()).build()).then().subscribe();
                     });
-                    return out.neverComplete();
-                });
+                    in.context().onClose(()-> startClient());
+                    return out.context().onClose();
+                })
+                .retry()
+                .block();
+        try {
+            Thread.sleep(1000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
